4e4686e0a130f632c1dac6e74bdb3a60fed330c0
Martin andersen[D[D[D[D[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[C <martinandersen3d@gmail.com>
2023-03-25 16:31:41 +0100

    init

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using ReturnUtils;
+
+class Program
 {
     public static void Main(string[] args)
     {
@@ -15,28 +17,6 @@
         if (divide.Failed) Console.WriteLine("Ohh Nooo - You devided with 0");
 
         var random = Random();
-    }
-
-    // A Sctruct that takes a Generic Type
-    // Note: Succes/Failed is set automaticly
-    public struct ReturnValue<T>
-    {
-        private T _value;
-        public T Value
-        {
-            get { return _value; }
-            set { _value = value; Success = true; }
-        }
-
-        public bool Success;
-        public bool Failed => !Success;
-
-        private Exception _error;
-        public Exception Error
-        {
-            get { return _error; }
-            set { _error = value; Success = false; }
-        }
     }
 
     // Define return type to INT
---
+++ ReturnValue.cs
@@ -0,0 +1,24 @@
+﻿namespace ClassLib.ReturnUtils
+{
+    // A Sctruct that takes a Generic Type
+    // Note: Succes/Failed is set automaticly
+    public struct ReturnValue<T>
+    {
+        private T _value;
+        public T Value
+        {
+            get { return _value; }
+            set { _value = value; Success = true; }
+        }
+
+        public bool Success;
+        public bool Failed => !Success;
+
+        private Exception _error;
+        public Exception Error
+        {
+            get { return _error; }
+            set { _error = value; Success = false; }
+        }
+    }
+}
